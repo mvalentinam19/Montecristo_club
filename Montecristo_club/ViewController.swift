6f055fe614ef8ccad0ab06167753752f924d4309
Santiago <ysantiago@unicauca.edu.co>
2020-02-27 15:44:31 -0500

    Initial Commit

---
+++ Montecristo_club/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Montecristo_club
+//
+//  Created by Enfasis on 2/26/20.
+//  Copyright © 2020 html. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var createAccountbutton: UIButton!
+    @IBOutlet weak var registerAccountbutton: UIButton!
+    
+    private let primarycolor = UIColor(red: 254/255, green: 254/255, blue: 254/255, alpha: 1);
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
